6c41cde96a38afd3cb1f524b424ad4b04886ae51
CakraDarma <baguscakraa02@gmail.com>
2023-01-24 14:27:40 +0800

    add commands for docker port

--- belajar-docker-dasar/5-docker-container-port.sh
+++ belajar-docker-dasar/5-docker-container-port.sh
@@ -1,4 +1,6 @@
-
+# install docker image
 docker image pull nginx:latest
-
+# create container for nginx
 docker container create --name contohnginx --publish 8080:80 nginx:latest
+# start container
+docker container start contohnginx
